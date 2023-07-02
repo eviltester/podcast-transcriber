ba5106486c95eb6dc66101469e9a0027a345f3e7
alan <alan@compendiumdev.co.uk>
2023-07-02 14:28:02 +0100

    crude download queue

--- downloads.py
+++ downloads.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 import os
 from urllib.parse import urlparse
@@ -33,6 +34,63 @@ class DownloadFile:
         self.url = url_to_download
 
 class DownloadQueue:
-    def __init__(self):
+    def __init__(self, cached_queue_file="", cached_downloaded_queue_file=""):
         self.to_download = []
         self.downloaded = []
+        self.cached_queue_file = cached_queue_file
+        self.cached_downloaded_queue_file = cached_downloaded_queue_file
+
+        if(cached_queue_file!=""):
+            # load the cached queue file
+            self.__load_csv_file_as_array_contents(cached_queue_file, self.to_download)
+        
+        if(cached_downloaded_queue_file!=""):
+            # load the cached queue file
+            self.__load_csv_file_as_array_contents(cached_downloaded_queue_file, self.downloaded)
+
+    def __load_csv_file_as_array_contents(self, filepath, an_array):
+        if(not os.path.exists(filepath)):
+            return
+        with open(filepath, newline='') as csvfile:
+            download_file_list_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in download_file_list_reader:
+                an_array.append(DownloadFile(row[0], row[1], row[2], row[3]))
+
+    def __save_array_contents_as_csv_file(self, filepath, an_array):
+        with open(filepath, 'w', newline='') as csvfile:
+            download_file_list_writer = csv.writer(csvfile, delimiter=',', quotechar='"')
+            for item in an_array:
+                download_file_list_writer.writerow([item.podcast_name, item.episode_title, item.download_folder, item.url])
+
+
+    def save_caches(self):
+        if(self.cached_queue_file!=""):
+            self.__save_array_contents_as_csv_file(self.cached_queue_file, self.to_download)
+
+        if(self.cached_downloaded_queue_file!=""):
+            self.__save_array_contents_as_csv_file(self.cached_downloaded_queue_file, self.downloaded)
+
+    def add(self, aDownloadFile: DownloadFile):
+        # do not add duplicate urls already in the downloaded queue
+        for item in self.downloaded:
+            if(item.url == aDownloadFile.url):
+                print ("Already downloaded " + item.url)
+                return
+    
+        # do not add duplicate urls already in the download queue
+        for item in self.to_download:
+            if(item.url == aDownloadFile.url):
+                print ("Already in download queue " + item.url)
+                return
+            
+        self.to_download.append(aDownloadFile)
+
+    def get_next(self) -> DownloadFile:
+        if len(self.to_download)>0:
+            return self.to_download[0]
+        else:
+            return None
+    
+    def mark_as_downloaded(self, aDownloadFile: DownloadFile):
+        self.to_download.remove(aDownloadFile)
+        self.downloaded.append(aDownloadFile)
--- transcriberapp.py
+++ transcriberapp.py
@@ -1,5 +1,7 @@
+# pyyaml just for printing for debugging convenience
+import yaml
 import os
-from downloads import download_if_not_exists
+from downloads import DownloadFile, DownloadQueue, download_if_not_exists
 from transcriber import transcribe
 
 #
@@ -9,10 +11,11 @@ from transcriber import transcribe
 
 '''
 Todo List
+TODO: download files into podcast specific named folders
 TODO: Given an list of RSS feeds parse and process
 TODO: Given an RSS feed parse and find new episodes, to add to download queue
 TODO: Given a list of urls in a download queue, download and parse the files
-- TODO: Create a basic queue list and process
+- Create a basic queue list and process
 - TODO: persists the queue to files to have history of downloads, this allows deleting mp3s after transcription
 
 - Given a url, download the mp3 and transcribe
@@ -20,18 +23,31 @@ TODO: Given a list of urls in a download queue, download and parse the files
 
 
 # Data and Configuration
-downloadUrl="https://downloads.pod.co/558da60d-be59-43be-afee-67c93f05c5e0/4c20f2bf-814a-47d3-90ca-42ede6325c2e.mp3"
 downloadPath = "/Users/alanrichardson/Downloads"
 outputPath = "/Users/alanrichardson/Documents/docs-git/dev/python/podcast-transcriptions"
 outputFileName = "testpodcast-transcription"
 whisperModel = "base"
 
+csvCaches = outputPath
+download_csv_cache = os.path.join(csvCaches, "download_queue.csv")
+downloaded_csv_cache = os.path.join(csvCaches, "downloaded_items.csv")
 
+download_queue = DownloadQueue(download_csv_cache, downloaded_csv_cache)
+download_queue.add(DownloadFile("Test Podcast", "episode one", downloadPath, "https://downloads.pod.co/558da60d-be59-43be-afee-67c93f05c5e0/4c20f2bf-814a-47d3-90ca-42ede6325c2e.mp3"))
+download_queue.add(DownloadFile("Test Podcast", "episode two", downloadPath, "https://downloads.pod.co/558da60d-be59-43be-afee-67c93f05c5e0/2c84ff05-8db6-4c4d-b975-24590b40ee54.mp3"))
 
-# download the next podcast
-inputAudioFile = download_if_not_exists(downloadUrl, downloadPath)
+next_download = download_queue.get_next()
+# download and transcribe the next podcast
+while next_download != None:
+    print(yaml.dump(download_queue, indent=2))
+    inputAudioFile = download_if_not_exists(next_download.url, next_download.download_folder)
+    # transcribe(inputAudioFile, outputPath, outputFileName)
+    download_queue.mark_as_downloaded(next_download)
+    download_queue.save_caches()
 
-transcribe(inputAudioFile, outputPath, outputFileName)
+    next_download = download_queue.get_next()
+
+    # print(yaml.dump(download_queue, indent=2))
 
 
 print("All Done")
