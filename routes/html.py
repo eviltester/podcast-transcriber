d215549c088e696b0a2b63a21c355b353cc43fb4
Alan Richardson <alan@compendiumdev.co.uk>
2025-07-28 22:07:32 +0100

    basic page templates to build on

--- routes/html.py
+++ routes/html.py
@@ -20,11 +20,4 @@ def index():
 @html_bp.route('/podcasts', methods=['GET'])
 def get_podcasts():
     feeds = get_feeds_list()
-    return render_template_string('''
-    <h1>RSS Feeds</h1>
-    <ul>
-        {% for feed in feeds %}
-            <li>{{ feed.feedname }} - {{ feed.feed_rss_url }}</li>
-        {% endfor %}
-    </ul>
-    ''', feeds=feeds)
+    return render_template('podcasts.html', feeds=feeds)
